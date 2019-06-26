d8e6e4da27afc321f85f5da92938e559e3f51954
Aiman Samad <aiman.samad@zemoga.com>
2019-06-26 10:33:37 -0500

    Added next config

--- apollo/client.js
+++ apollo/client.js
@@ -1,10 +1,13 @@
 import ApolloClient from 'apollo-boost';
 import fetch from 'isomorphic-fetch';
-import config from '../appConfig';
+import getConfig from 'next/config';
+// import config from '../appConfig';
+
+const { publicRuntimeConfig } = getConfig();
 
 const client = new ApolloClient({
   fetch,
-  uri: `${config.HOST}/graphql`,
+  uri: `${publicRuntimeConfig.HOST}/graphql`,
 });
 
 export default client;
